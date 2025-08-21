4fab498d4703056124c9ab9d8561eefa5801f9a1
theamirm <theamirm@gmail.com>
2025-08-21 22:40:05 +0330

    style: update Logo component dimensions and text description

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function Logo() {
   return (
-    <div className="text-center mb-2 relative h-48">
+    <div className="text-center mb-2 relative h-44">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div className="w-52 h-52 rounded-full bg-brpink-100/20 opacity-50 blur-3xl"></div>
       </div>
@@ -12,13 +12,13 @@ export function Logo() {
           alt="Todook Logo"
           width={100}
           height={100}
-          className="h-36 w-auto drop-shadow-lg drop-shadow-brpink-100/10"
+          className="h-32 w-auto drop-shadow-lg drop-shadow-brpink-100/10"
           priority
         />
       </div>
       <h1 className="sr-only">Todook</h1>
       <p className="text-brgray-100 text-sm font-medium italic">
-        Simple, yet reliable
+        Todo list, for Barook!{" "}
       </p>
     </div>
   );

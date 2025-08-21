8022f6339ae92759855ffbb75c8fd9ac7e2a658d
theamirm <theamirm@gmail.com>
2025-08-21 16:34:25 +0330

    style: update Logo component colors and shadow effects

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <div className="text-center mb-2 relative h-48">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="w-52 h-52 rounded-full bg-brgray-100/20 opacity-50 blur-3xl"></div>
+        <div className="w-52 h-52 rounded-full bg-brpink-100/20 opacity-50 blur-3xl"></div>
       </div>
       <div className="flex justify-center mb-0">
         <Image
@@ -12,7 +12,7 @@ export function Logo() {
           alt="Todook Logo"
           width={100}
           height={100}
-          className="h-36 w-auto drop-shadow-sm"
+          className="h-36 w-auto drop-shadow-lg drop-shadow-brpink-100/10"
           priority
         />
       </div>

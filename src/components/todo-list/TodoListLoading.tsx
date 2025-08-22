35fb419f47f1f76b465a9225022548fc5925da6e
theamirm <theamirm@gmail.com>
2025-08-23 00:12:00 +0330

    style: further polish svg visuals

--- src/components/todo-list/TodoListLoading.tsx
+++ src/components/todo-list/TodoListLoading.tsx
@@ -10,11 +10,16 @@ export const TodoListLoading = React.memo(function TodoListLoading() {
       data-testid="todo-list-loading"
     >
       <div className="mb-4">
-        <LoadingSpinner size="lg" className="!text-brgray-100 w-12 h-12" />
+        <LoadingSpinner
+          size="lg"
+          className="!text-white/90 w-12 h-12 drop-shadow-[0_0_2px_rgba(255,255,255,0.15),0_0_4px_rgba(255,255,255,0.2),0_0_20px_rgba(0,0,0,0.1),0_0_8px_rgba(0,0,0,0.15)]"
+        />
       </div>
-      <p className="text-lg font-medium mb-2">Loading todos...</p>
-      <p className="text-sm text-brgray-100 font-normal">
-        Please wait while we fetch your todos
+      <p className="text-lg font-medium mb-2 !text-brgray-300/90">
+        Loading todos
+      </p>
+      <p className="text-xs font-semibold text-brgray-100/80 ">
+        Please wait a moment
       </p>
     </div>
   );
